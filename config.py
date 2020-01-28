750ffd287d7cbe06f8ff473189605d83242f5dbb
Xiao kuang <kuangx@oregonstate.edu>
2020-01-28 14:04:14 -0800

    Completed chapter 5 of flask megatutorial

---
+++ config.py
@@ -0,0 +1,19 @@
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+
+# class Config(object):
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+#     SQALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+#         'sqlite:///' + os.path.join(basedir, 'app.db')
+#     SQALCHEMY_TRACK_MODIFICATIONS = False
+    
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
